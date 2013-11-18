f32e04702824345f30a4f722f2ba0d3edf8bb9a4
izobs <ivincentlin@gmail.com>
2013-11-18 21:44:41 +0800

    chang code style

--- README.md
+++ README.md
@@ -4,7 +4,4 @@
 - module文件夹存放驱动代码
 - document文件夹存放相关的datasheet等文档
 
-###使用:
-
-> git clone git://gitcafe.com/iZobs/TQ2440-at24c02.git 
 
--- module/.at24c02.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/izobs/Linux_c/arm-at24c02/module/at24c02.ko := arm-linux-ld -EL -r   --build-id -o /home/izobs/Linux_c/arm-at24c02/module/at24c02.ko /home/izobs/Linux_c/arm-at24c02/module/at24c02.o /home/izobs/Linux_c/arm-at24c02/module/at24c02.mod.o
